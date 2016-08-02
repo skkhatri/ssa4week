379e3cd55122af5d550792b7c52cf5423e3498c8
swarup <swarupkhatri@gmail.com>
2016-08-02 08:01:36 -0400

    first commit

---
+++ example.js
@@ -0,0 +1,44 @@
+
+<!Doctype HTML>
+<html>
+<head>
+<title>Hello, World</title>
+
+</head>
+
+<body>
+<h2>Hello</h2>
+<a class="class1" href="https://en.wikipedia.org/wiki/Duck">Ducks</a>
+</body>
+
+<script>
+
+var x=3;
+
+function foo(a, b) {
+  return a + b;
+
+for (i =0; i < 10; i++) {
+   console.log(i)
+
+
+}
+
+var a = [];    //array A
+a[0] = 35;
+
+
+
+var f = foo;
+foo(6, 5)      //11
+
+
+
+
+
+
+
+
+
+</script>
+</html>
